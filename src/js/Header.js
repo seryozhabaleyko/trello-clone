8c970bc124201a72275a71e300e0151012e20025
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-13 19:04:15 +0300

    Update Header.js

--- src/js/Header.js
+++ src/js/Header.js
@@ -2,11 +2,9 @@
 
 import DOMHelpers from './helpers/DOMHelpers.js';
 
-const Header = () => {
+const { createElement } = DOMHelpers();
 
-    const {
-        createElement
-    } = DOMHelpers();
+const Header = () => {
 
     const header = () => {
         const $header = createElement('header');
