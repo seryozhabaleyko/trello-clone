304f8b52872435e62d369eab17c5cf617c34db1b
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-13 19:03:54 +0300

    Update index.js

--- src/js/Board/index.js
+++ src/js/Board/index.js
@@ -1,33 +1,49 @@
 'use strict';
 
 import DOMHelpers from '../helpers/DOMHelpers.js';
-import Add from './Add.js';
+import header from './boardHeader.js';
+import addList from './addList.js';
 import List from './List.js';
+import Store from '../Store.js';
 
-const add = Add();
-const list = List();
+const { insert } = List();
 
-
-const {
-    createElement
-} = DOMHelpers();
+const { createElement } = DOMHelpers();
+const store = Store();
 
 const board = (obj) => {
+
+    /* obj.lists = [
+        { id: 1, title: 'Название 1',
+            cards: [
+                { id: 1, text: 'систем управления контентом'},
+                { id: 2, text: 'что видишь, то и получишь'},
+                { id: 3, text: 'используемая для обеспечения и организации совместного процесса'},
+            ]
+        },
+        { id: 2, title: 'Название 2' },
+        { id: 3, title: 'Название 3',
+            cards: [
+                { id: 1, text: 'позиция для вставки текста относительно элемента'}
+            ]
+        },
+        { id: 4, title: 'Название 4' }
+    ]; */
+
     const $main = createElement('div', '#main');
-    $main.style.background = obj.bg;
 
     const $board = createElement('div', '#board');
     const $lists = createElement('div', '#lists');
+    $lists.setAttribute('data-lists-id', obj.id);
 
     if (obj.lists) {
         obj.lists.forEach(list => {
-            
+            $lists.appendChild(insert(list));
         });
     }
-    //$lists.appendChild();
 
-    $board.append($lists, add.list());
-    $main.appendChild($board);
+    $board.append($lists, addList());
+    $main.append(header(), $board);
 
     return $main;
 }
