95931bc4dc0591b3960333c31781edc5ccc9ac8f
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-13 19:04:44 +0300

    Update Footer.js

--- src/js/List/Footer.js
+++ src/js/List/Footer.js
@@ -1,10 +1,36 @@
 'use strict';
 
-const Footer = () => {
+import DOMHelpers from '../helpers/DOMHelpers.js';
 
-    return {
+const {
+    createElement,
+    show,
+    hide
+} = DOMHelpers();
 
-    };
+function showFormHandler(root, form) {
+    hide(root);
+    show(form);
+    const $input = form.querySelector('.card-add-text');
+    $input.focus();
 }
 
-export default Footer;
+const showForm = (root, form) => {
+    const $button = createElement('button', '.add-card');
+    $button.setAttribute('type', 'button');
+    $button.insertAdjacentText('afterbegin', '+ Добавить карточку');
+
+    $button.addEventListener('click', showFormHandler.bind(this, root, form), false);
+
+    return $button;
+};
+
+const footer = (form) => {
+    const $footer = createElement('div', '.list-footer');
+
+    $footer.appendChild(showForm($footer, form));
+
+    return $footer;
+};
+
+export default footer;
