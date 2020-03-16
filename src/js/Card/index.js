acd69f55addf28035024b6717d45264fbb5ac3ae
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-16 15:55:33 +0300

    Update index.js

--- src/js/Card/index.js
+++ src/js/Card/index.js
@@ -1,18 +1,28 @@
 'use strict';
 
 import DOMHelpers from '../helpers/DOMHelpers.js';
+import drag from './drag&drop.js';
+
+const {
+    createElement,
+    on
+} = DOMHelpers();
 
 const Card = () => {
-
-    const {
-        createElement
-    } = DOMHelpers();
 
     const insert = (obj) => {
         const $card = createElement('div', '.card');
         $card.setAttribute('draggable', 'true');
         $card.setAttribute('data-card-id', obj.id);
         $card.insertAdjacentHTML('afterbegin', obj.text);
+
+        function cardHandler() {
+            console.log(123123);
+        }
+
+        on($card, 'click', cardHandler, false);
+
+        drag($card);
 
         return $card;
     };
