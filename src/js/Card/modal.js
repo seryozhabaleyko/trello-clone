1955b88907f0a9cc7088263b8433efc6afcc15c0
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-16 15:55:36 +0300

    Create modal.js

---
+++ src/js/Card/modal.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import DOMHelpers from '../helpers/DOMHelpers.js';
+
+const { createElement } = DOMHelpers();
+
+const toggle = () => {
+
+};
+
+const hide = () => {
+
+};
+
+const show = () => {
+
+};
+
+const modal = () => {
+    const $modal = createElement('div', '.modal');
+    return $modal;
+};
+
+export default modal;
