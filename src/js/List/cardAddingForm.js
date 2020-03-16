8a961ed68f6c7d6efa8ff919dd840245eeb75de8
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-16 15:56:08 +0300

    Update cardAddingForm.js

--- src/js/List/cardAddingForm.js
+++ src/js/List/cardAddingForm.js
@@ -1,28 +1,38 @@
 'use strict';
 
 import DOMHelpers from '../helpers/DOMHelpers.js';
+import Store from '../Store.js';
+import Card from '../Card/index.js';
+import { hideFormHandler } from './footer.js';
 
 const {
     createElement,
     show,
-    hide
+    hide,
+    on,
+    off
 } = DOMHelpers();
+
+const store = Store();
+const card = Card();
 
 function closeHandler(root, input) {
     hide(root);
     input.value = '';
     const $footer = root.parentNode.nextSibling;
     show($footer);
+
+    off(document, 'click', hideFormHandler, true);
 }
 
 const close = (root, input) => {
     const $close = createElement('button', '.add-card-close');
     $close.setAttribute('type', 'button');
     $close.insertAdjacentHTML('afterbegin', `
-            <svg viewBox="0 0 24 24" width="24" height="24">
-                <path d="M13.476 12.04l4.248-4.248a.992.992 0 1 0-1.402-1.403l-4.248 4.248L7.787 6.35a.992.992 0 0 0-1.402 1.403l4.286 4.286-4.26 4.261a.992.992 0 1 0 1.402 1.403l4.26-4.261 4.223 4.222a.992.992 0 1 0 1.403-1.402l-4.223-4.223z"></path>
-            </svg>
-        `);
+        <svg viewBox="0 0 24 24" width="24" height="24">
+            <path d="M13.476 12.04l4.248-4.248a.992.992 0 1 0-1.402-1.403l-4.248 4.248L7.787 6.35a.992.992 0 0 0-1.402 1.403l4.286 4.286-4.26 4.261a.992.992 0 1 0 1.402 1.403l4.26-4.261 4.223 4.222a.992.992 0 1 0 1.403-1.402l-4.223-4.223z"></path>
+        </svg>
+    `);
 
     $close.addEventListener('click', closeHandler.bind(this, root, input));
 
