7a8010b1e5a511b9c92ed10c76ce8ad8b09365ab
Серёжа Балейко <seryozha.baleyko@gmail.com>
2020-03-18 15:20:11 +0300

    Update Header.js

--- src/js/Header.js
+++ src/js/Header.js
@@ -4,16 +4,29 @@ import DOMHelpers from './helpers/DOMHelpers.js';
 
 const { createElement } = DOMHelpers();
 
-const Header = () => {
+const logo = () => {
+    const $logo = createElement('a', '#logo');
+    $logo.href = '/';
+    $logo.textContent = 'kanban';
+    return $logo;
+};
 
-    const header = () => {
-        const $header = createElement('header');
-        return $header;
-    }
+const boards = () => {
+    const $boards = createElement('button', '.btn-boards');
+    $boards.textContent = 'доски';
+    return $boards;
+};
 
-    return {
-        header
-    };
+const home = () => {
+    const $home = createElement('button', 'btn-home');
+
+    return $home;
+};
+
+const header = () => {
+    const $header = createElement('header');
+    $header.append(home(), boards(), logo());
+    return $header;
 }
 
-export default Header;
+export default header;
